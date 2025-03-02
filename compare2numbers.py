ba5ecbd84c16afb6c7e64d6625b4e449566af333
Pawan Shaji <121746814+Morpheus-pwn@users.noreply.github.com>
2025-03-02 14:42:36 +0530

    Delete compare2numbers.py

--- compare2numbers.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter first number to compare:"))
-
-b=int(input("enter second number to compare:"))
-
-if a>b:
-    print(a,"is greater than",b)
-else:
-    print(b,"is greater than",a)
-    
