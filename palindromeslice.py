8f9acbdb43bd5ae8628d5387717b75ff32fa3e11
Pawan Shaji <121746814+Morpheus-pwn@users.noreply.github.com>
2025-03-02 14:44:41 +0530

    Delete palindromeslice.py

--- palindromeslice.py
+++
@@ -1,7 +0,0 @@
-string = input("Enter a string: ")
-reversed_string = string[::-1]
-
-if string == reversed_string:
-    print(string, "is a palindrome")
-else:
-    print(string, "is not a palindrome")
