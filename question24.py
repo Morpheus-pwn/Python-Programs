c4282492f1613451b5a48d732fccf38f1b5327c7
Pawan Shaji <121746814+Morpheus-pwn@users.noreply.github.com>
2025-03-02 14:45:39 +0530

    Delete question24.py

--- question24.py
+++
@@ -1,9 +0,0 @@
-for num in range(100, 1001):
-    sum_of_digits = 0
-    temp = num
-    while temp > 0:
-        digit = temp % 10
-        sum_of_digits += digit
-        temp //= 10
-    if sum_of_digits % 9 == 0:
-        print(num)
