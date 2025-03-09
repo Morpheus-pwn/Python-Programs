1c927d8c948553950547c1f1cb6c07be1b6958d6
Pawan Shaji <121746814+Morpheus-pwn@users.noreply.github.com>
2025-03-09 21:58:13 +0530

    Delete primefactors.py

--- primefactors.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number: "))
-
-print("Prime factors of", num, "are:")
-for i in range(2, int(num**0.5) + 1):
-    while num % i == 0:
-        print(i)
-        num //= i
-if num > 1:
-    print(num)
